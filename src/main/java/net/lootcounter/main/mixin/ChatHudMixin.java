4a9862443fad7a0c54c0831a9639991464275462
Vladomeme <84517135+Vladomeme@users.noreply.github.com>
2022-10-10 23:16:52 +0500

    Update ChatHudMixin.java

--- src/main/java/net/lootcounter/main/mixin/ChatHudMixin.java
+++ src/main/java/net/lootcounter/main/mixin/ChatHudMixin.java
@@ -13,28 +13,53 @@ import net.minecraft.text.Text;
 public class ChatHudMixin {
 	@Inject(method = "Lnet/minecraft/client/gui/hud/ChatHud;addMessage(Lnet/minecraft/text/Text;)V", at = @At("HEAD"))
 	void addMessage(Text message, CallbackInfo ci) {
-		switch(message.getString()) {
+		String chatmessage = message.getString();
+		if(chatmessage.startsWith("Transferring you to")) {
+			CounterInit.ResetCounter();
+			CounterInit.currentDimension= "none";
+			CounterInit.ResetFloorCounter();
+			switch(chatmessage) {
+			case "Transferring you to verdant":{
+				CounterInit.currentDimension="verdant";
+				CounterInit.maxchests="42";
+			}
+			break;
+			case "Transferring you to sanctum":{
+				CounterInit.currentDimension="sanctum";
+				CounterInit.maxchests="65";
+			}
+			break;
+			case "Transferring you to corridors":{
+				CounterInit.currentDimension="corridors";
+			}
+			break;
+			case "Transferring you to mist":{
+				CounterInit.currentDimension="mist";
+				CounterInit.maxchests="40";
+			}
+			break;
+			case "Transferring you to remorse":{
+				CounterInit.currentDimension="remorse";
+				CounterInit.maxchests="62";
+			}
+			break;
+			}
+		}
+		switch(chatmessage) {
 		case "[-_-] Is this the run that never ends? Yes it goes on and on my friend.": {
 			CounterInit.maxchests="∞";
-			CounterInit.ResetCounter();
 		}
 		break;
 		case "[-_-] Getting saved is standard now? Adventurers these days...": {
 			CounterInit.maxchests="21";
-			CounterInit.ResetCounter();
 		}
 		break;
 		case "[-_-] Practice makes perfect. Take all the time you need.": {
 			CounterInit.maxchests="21";
-			CounterInit.ResetCounter();
 		}
 		break;
 		}
-		if(message.getString().startsWith("Transferring you to")) {
-			CounterInit.ResetCounter();
-			CounterInit.ResetFloorCounter();
-		}
-		if(message.getString().startsWith("[Corridors] You have advanced")) {
+		if(chatmessage.startsWith("[Corridors] You have advanced")) {
 			CounterInit.ResetFloorCounter();
 		}
 	}
